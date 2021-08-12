3bed603ec7163eee9246ecba472b7b637fc4a27e
Henry Owens <henry@owens.codes>
2021-08-12 21:53:55 +0100

    use async await to fetch

--- src/http/get-suggestions/index.js
+++ src/http/get-suggestions/index.js
@@ -9,17 +9,12 @@ exports.handler = async function http(req) {
     headers: myHeaders,
     redirect: 'follow'
     };
-
-    let resultsJSON;
         
-    fetch("https://www.google.com/complete/search?q=peloton&cp=1&client=gws-wiz&xssi=t", requestOptions)
-    .then(response => response.text())
-    .then(result => resultsJSON = result)
-    .catch(error => console.log('error', error));
+    const suggestions = await fetch("https://www.google.com/complete/search?q=peloton&cp=1&client=gws-wiz&xssi=t", requestOptions)
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
         statusCode: 200,
-        body: resultsJSON
+        body: suggestions
     }
 }
