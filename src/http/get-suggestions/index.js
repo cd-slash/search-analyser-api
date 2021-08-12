8dca6f6fe6dbfd6d6f74dba7097c6f945196b8db
Henry Owens <henry@owens.codes>
2021-08-12 21:58:48 +0100

    update fetch logic

--- src/http/get-suggestions/index.js
+++ src/http/get-suggestions/index.js
@@ -10,7 +10,8 @@ exports.handler = async function http(req) {
     redirect: 'follow'
     };
         
-    const suggestions = await fetch("https://www.google.com/complete/search?q=peloton&cp=1&client=gws-wiz&xssi=t", requestOptions)
+    const response = await fetch("https://www.google.com/complete/search?q=peloton&cp=1&client=gws-wiz&xssi=t", requestOptions);
+    const suggestions = await response.text();
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
