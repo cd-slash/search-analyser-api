df5c2a70f46bd2c6b242cef0bb0407d6eb3563c3
Henry Owens <henry@owens.codes>
2021-08-12 17:28:33 +0100

    rename JSON results for clarity

--- http/get-competitors/index.js
+++ http/get-competitors/index.js
@@ -1,10 +1,10 @@
 exports.handler = async function http(req) {
 
-    let jsonString = {1: "SoulCycle", 2: "Wattbike"}
+    let resultsJSON = {1: "SoulCycle", 2: "Wattbike"}
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
         statusCode: 200,
-        body: JSON.stringify(jsonString)
+        body: JSON.stringify(resultsJSON)
     }
 }
